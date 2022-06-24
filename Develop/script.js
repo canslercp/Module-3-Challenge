57ee34839d2f0f4e5142df4b131891d0b0490f35
canslercp <canslercp@gmail.com>
2022-06-23 21:31:10 -0400

    rewrote if statements, created arrays, promt user for password criteria,and  create return prompt.

--- Develop/script.js
+++ Develop/script.js
@@ -16,31 +16,67 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-var generatePassword = function() {
+var generatePassword = function () {
+  // Define global variables and arrys
+    var i = 0;
+
+    var num = [];
+    for(var i = 0; i < 128; i++){
+      num.push(i);
+    }
+    console.log(num);
+
+    var SP = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","~"]
+    // Use iteration to create an uppercase alphabet array
+    var alphaUC = Array.from(Array(26)).map((e, i) => i + 65);
+    var UC = alphaUC.map((x) => String.fromCharCode(x));
+    // Use iteration to create an uppercase alphabet array
+    var alphaLC = Array.from(Array(26)).map((e, i) => i + 97);
+    var LC = alphaLC.map((x) => String.fromCharCode(x));
+    
   // Ask user for their choice
-  var userChoice = window.prompt("Enter length and or character type");
+  var userChoice = window.prompt("How many characters would you like your password to conatin?");
 
   // If user pressed Cancel, immediately end function
   if (!userChoice) {
     return;
   }
+  //Prompt the user for the password criteria
+  //    a. password legnth 8 <= i <=128
+  //    b.confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  if (userChoice >= 8 && userChoice <= 128) {
 
-  // Convert to uppercase to make comparisons easier
-  userChoice = userChoice.toUpperCase();
+    var useSP = window.confirm("Click OK to confirm using special characters.");
+    var useNum = window.confirm("Click OK to confirm using numbers.");
+    var useLC = window.confirm("Click OK to confirm using lowercase characters.");
+    var useUC = window.confirm("Click OK to confirm using upercase characters,");
 
-  // If choice is L
-  if (userChoice === L) {
-    var userLength = window.prompt("Enter a character length of at least 8 but no more than 128")
-  // If choice is C
-  } else if (userChoice === C) {
-    var charType = window.prompt("Would you like to inclue lowercase?")
-        // Not sure how to finish this just yet
-  // If user choice is LC
-  } else if (userChoice === LC || userChoice === CL) {
-    var userLength = window.prompt("Enter a character length of at least 8 but no more than 128")
-    var charType = window.prompt("Would you like to inclue lowercase?")
-        // Not sure how to finish this just yet
-  } else
-   //Need to send user back to the original promt after a message saying the need to choose criteria
+  //3. Generate password????????????????
+    if (useSP === true){}
+      // var numSP = 
+      //for (var i=0; i<userChoice; i++) {
+      //  var rnum = Math.floor(Math.random() * SP.length);
+      //  console.log(rnum);
+      //}
+
+    else if (useSP === true && useNum === true){}
+    else if (useSP === true && useNum === true && useLC ===true){}
+    else if (useSP === true && useNum === true && useLC ===true && useUC === true){}
+
+    //return prompt if at least one character type is not selected
+    else {
+      window.alert("At least one character type should be selected");
+      return;
+    }
+  }
+
+  //return prompt if user inputs a length less than 8 or greater than 128
+  else {
+    window.alert("Please choose a character length of at least 8, but no more than 128");
+    return;
+  }
+
+  //4. Display password to the page
+  return "Generated password goes here!"
 
 }
